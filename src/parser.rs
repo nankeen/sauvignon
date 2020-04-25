e3a1a84a08f203b6459aaeb9422817e4cbb4cd1f
CK <nankeen@users.noreply.github.com>
2020-04-25 22:34:33 +0100

    Implemented expression parsing with precedence

--- src/parser.rs
+++ src/parser.rs
@@ -67,11 +67,11 @@ impl<'a> Parser<'a> {
     fn parse_return_statement(&mut self) -> Result<Statement, String> {
         self.next_token();
 
-        // TODO: skipping expressions
-        while self.cursor != Token::Semicolon {
+        let expr = self.parse_expression(Precedence::Lowest)?;
+        if let Some(Token::Semicolon) = self.lexer.peek() {
             self.next_token();
         }
-        Ok(Statement::ReturnStatement(Expression::Empty))
+        Ok(Statement::ReturnStatement(expr))
     }
 
     fn parse_expression_statement(&mut self) -> Result<Statement, String> {
@@ -93,13 +93,55 @@ impl<'a> Parser<'a> {
         })
     }
 
-    fn parse_expression(&mut self, _precedence: Precedence) -> Result<Expression, String> {
-        match self.cursor {
+    fn parse_expression(&mut self, precedence: Precedence) -> Result<Expression, String> {
+        let mut left = match self.cursor {
             Token::Ident(_) => Ok(Expression::Ident(self.cursor.clone())),
             Token::IntLiteral(i) => Ok(Expression::IntLiteral(i)),
             Token::Bang => self.parse_prefix_expr(),
             Token::Minus => self.parse_prefix_expr(),
             _ => Err(format!("unexpected token {:?} in expression", self.cursor)),
+        }?;
+
+        while precedence < Parser::map_precedence(self.lexer.peek().unwrap()) {
+            left = match self.lexer.peek() {
+                Some(Token::Semicolon) => break,
+                Some(Token::Plus) => self.parse_infix_expr(left)?,
+                Some(Token::Minus) => self.parse_infix_expr(left)?,
+                Some(Token::Slash) => self.parse_infix_expr(left)?,
+                Some(Token::Asterisk) => self.parse_infix_expr(left)?,
+                Some(Token::Equal) => self.parse_infix_expr(left)?,
+                Some(Token::NotEqual) => self.parse_infix_expr(left)?,
+                Some(Token::LessThan) => self.parse_infix_expr(left)?,
+                Some(Token::GreaterThan) => self.parse_infix_expr(left)?,
+                _ => break,
+            };
+        }
+        Ok(left)
+    }
+
+    fn parse_infix_expr(&mut self, left: Expression) -> Result<Expression, String> {
+        self.next_token();
+        let token = self.cursor.clone();
+        let precedence = Parser::map_precedence(&self.cursor);
+        self.next_token();
+        Ok(Expression::InfixExpression {
+            left: Box::new(left),
+            operator: token,
+            right: Box::new(self.parse_expression(precedence)?),
+        })
+    }
+
+    fn map_precedence(token: &Token) -> Precedence {
+        match token {
+            Token::Equal => Precedence::Equality,
+            Token::NotEqual => Precedence::Equality,
+            Token::LessThan => Precedence::Comparison,
+            Token::GreaterThan => Precedence::Comparison,
+            Token::Plus => Precedence::Sum,
+            Token::Minus => Precedence::Sum,
+            Token::Slash => Precedence::Product,
+            Token::Asterisk => Precedence::Product,
+            _ => Precedence::Lowest,
         }
     }
 
@@ -107,15 +149,12 @@ impl<'a> Parser<'a> {
         self.expect_token(Token::Ident("".to_string()))?;
         let ident = self.cursor.clone();
         self.expect_token(Token::Assign)?;
-
-        // TODO: skipping expressions
-        while self.cursor != Token::Semicolon {
+        self.next_token();
+        let expr = self.parse_expression(Precedence::Lowest)?;
+        if let Some(Token::Semicolon) = self.lexer.peek() {
             self.next_token();
         }
-        Ok(Statement::LetStatement {
-            ident,
-            expr: Expression::Empty,
-        })
+        Ok(Statement::LetStatement { ident, expr })
     }
 }
 
@@ -126,24 +165,34 @@ mod tests {
 
     #[test]
     fn test_let_statements() -> Result<(), String> {
-        let input = "let x = ;
+        let input = "let x = -1;
         let y = 10;
-        let foobar = 838383;";
+        let foobar = 838383 + 1;";
         let lexer = Lexer::new(input);
         let mut parser = Parser::new(lexer);
 
         let program = parser.parse_program()?;
         let mut statements = program.iter();
 
-        // TODO: Update test cases with proper expressions
         let tests = [
             Statement::LetStatement {
                 ident: Token::Ident("x".to_string()),
-                expr: Expression::Empty,
+                expr: Expression::PrefixExpression {
+                    operator: Token::Minus,
+                    right: Box::new(Expression::IntLiteral(1)),
+                },
             },
             Statement::LetStatement {
                 ident: Token::Ident("y".to_string()),
-                expr: Expression::Empty,
+                expr: Expression::IntLiteral(10),
+            },
+            Statement::LetStatement {
+                ident: Token::Ident("foobar".to_string()),
+                expr: Expression::InfixExpression {
+                    left: Box::new(Expression::IntLiteral(838383)),
+                    operator: Token::Plus,
+                    right: Box::new(Expression::IntLiteral(1)),
+                },
             },
         ];
 
@@ -158,7 +207,7 @@ mod tests {
 
     #[test]
     fn test_return_statements() -> Result<(), String> {
-        let input = "return 5;
+        let input = "return -5;
         return 10;
         return 993322;";
         let lexer = Lexer::new(input);
@@ -167,10 +216,14 @@ mod tests {
         let program = parser.parse_program()?;
         let mut statements = program.iter();
 
-        // TODO: Update test cases with proper expressions
+        // Update test cases with proper expressions
         let tests = [
-            Statement::ReturnStatement(Expression::Empty),
-            Statement::ReturnStatement(Expression::Empty),
+            Statement::ReturnStatement(Expression::PrefixExpression {
+                operator: Token::Minus,
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ReturnStatement(Expression::IntLiteral(10)),
+            Statement::ReturnStatement(Expression::IntLiteral(993322)),
         ];
 
         for test in &tests {
@@ -244,6 +297,70 @@ mod tests {
             Statement::ExpressionStatement(Expression::PrefixExpression {
                 operator: Token::Minus,
                 right: Box::new(Expression::IntLiteral(15)),
+            }),
+        ];
+
+        for test in &tests {
+            match statements.next() {
+                Some(tok) => assert_eq!(*test, *tok),
+                None => panic!("unexpected parser error: returned None"),
+            };
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_infix_expr() -> Result<(), String> {
+        let input = "5+5; 5-5;
+        5*5; 5 / 5;
+        5 > 5; 5 < 5;
+        5 == 5; 5 != 5";
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program()?;
+        let mut statements = program.iter();
+
+        let tests = [
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::Plus,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::Minus,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::Asterisk,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::Slash,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::GreaterThan,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::LessThan,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::Equal,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
+            }),
+            Statement::ExpressionStatement(Expression::InfixExpression {
+                operator: Token::NotEqual,
+                left: Box::new(Expression::IntLiteral(5)),
+                right: Box::new(Expression::IntLiteral(5)),
             }),
         ];
 
