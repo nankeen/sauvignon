8adaa29d6393ee779fb56192592ff1502d4294b8
CK <nankeen@users.noreply.github.com>
2020-04-29 19:29:52 +0100

    Implemented infix expressions

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -38,7 +38,42 @@ impl Evaluator {
                 let eval_r = self.eval_expression(right)?;
                 self.eval_prefix_expr(operator, &eval_r)
             }
+            Expression::InfixExpression {
+                left,
+                right,
+                operator,
+            } => {
+                let eval_l = self.eval_expression(left)?;
+                let eval_r = self.eval_expression(right)?;
+                self.eval_infix_expr(operator, &eval_l, &eval_r)
+            }
             _ => Err("not implemented"),
+        }
+    }
+
+    fn eval_infix_expr(&self, op: &Token, left: &Object, right: &Object) -> Result<Object, &str> {
+        match op {
+            Token::Plus => match (left, right) {
+                (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l + r)),
+                _ => Err("expected integer for infix plus"),
+            },
+            Token::Minus => match (left, right) {
+                (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l - r)),
+                _ => Err("expected integer for infix minus"),
+            },
+            Token::Asterisk => match (left, right) {
+                (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l * r)),
+                _ => Err("expected integer for infix times"),
+            },
+            Token::Slash => match (left, right) {
+                (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l / r)),
+                _ => Err("expected integer for infix divide"),
+            },
+            Token::Equal => Ok(Object::Boolean(left == right)),
+            Token::NotEqual => Ok(Object::Boolean(left != right)),
+            Token::LessThan => Ok(Object::Boolean(left < right)),
+            Token::GreaterThan => Ok(Object::Boolean(left > right)),
+            _ => Err("unknown infix operator"),
         }
     }
 
@@ -101,5 +136,31 @@ mod tests {
 
         eval_compare("-32", Object::Integer(-32));
         eval_compare("-1024", Object::Integer(-1024));
+    }
+
+    #[test]
+    fn test_eval_infix_int() {
+        eval_compare("32+32", Object::Integer(64));
+        eval_compare("8+16", Object::Integer(24));
+        eval_compare("8-16", Object::Integer(-8));
+        eval_compare("2*16", Object::Integer(32));
+        eval_compare("16/2", Object::Integer(8));
+        eval_compare("3 + 3 * 8", Object::Integer(27));
+        eval_compare("(3 + 3) * 8", Object::Integer(48));
+        eval_compare("(13 + 3) / 8", Object::Integer(2));
+        eval_compare("13 + 24 / 8", Object::Integer(16));
+    }
+
+    #[test]
+    fn test_eval_infix_bool() {
+        eval_compare("true == true", Object::Boolean(true));
+        eval_compare("true != false", Object::Boolean(true));
+        eval_compare("true != false == false", Object::Boolean(false));
+
+        eval_compare("32 == 32", Object::Boolean(true));
+        eval_compare("32 != 32", Object::Boolean(false));
+        eval_compare("8 < 16", Object::Boolean(true));
+        eval_compare("8 > 16", Object::Boolean(false));
+        eval_compare("2 > 16 == false", Object::Boolean(true));
     }
 }
