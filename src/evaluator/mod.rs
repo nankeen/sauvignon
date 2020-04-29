ded74b58c6a508b9fb71e480a98e5b8be2f107c5
CK <nankeen@users.noreply.github.com>
2020-04-29 03:10:18 +0100

    Implemented prefix and boolean feature to evaluator

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 mod object;
 use crate::ast::{Expression, Statement};
+use crate::lexer::Token;
 use object::*;
 
 pub struct Evaluator {}
@@ -32,7 +33,36 @@ impl Evaluator {
     fn eval_expression(&self, expr: &Expression) -> Result<Object, &str> {
         match expr {
             Expression::IntLiteral(i) => Ok(Object::Integer(*i)),
+            Expression::BoolLiteral(b) => Ok(Object::Boolean(*b)),
+            Expression::PrefixExpression { right, operator } => {
+                let eval_r = self.eval_expression(right)?;
+                self.eval_prefix_expr(operator, &eval_r)
+            }
             _ => Err("not implemented"),
+        }
+    }
+
+    fn eval_prefix_expr(&self, op: &Token, right: &Object) -> Result<Object, &str> {
+        match op {
+            Token::Bang => self.eval_bang_prefix(right),
+            Token::Minus => self.eval_minus_prefix(right),
+            _ => Err("not implemented"),
+        }
+    }
+
+    fn eval_minus_prefix(&self, right: &Object) -> Result<Object, &str> {
+        match right {
+            Object::Integer(i) => Ok(Object::Integer(-i)),
+            _ => Ok(Object::Null),
+        }
+    }
+
+    fn eval_bang_prefix(&self, right: &Object) -> Result<Object, &str> {
+        match right {
+            Object::Boolean(true) => Ok(Object::Boolean(false)),
+            Object::Boolean(false) => Ok(Object::Boolean(true)),
+            Object::Null => Ok(Object::Boolean(true)),
+            _ => Ok(Object::Boolean(false)),
         }
     }
 }
@@ -53,8 +83,23 @@ mod tests {
     }
 
     #[test]
-    fn test_eval_integer_expression() {
+    fn test_eval_simple_expression() {
         eval_compare("32", Object::Integer(32));
         eval_compare("1024", Object::Integer(1024));
+        eval_compare("true", Object::Boolean(true));
+        eval_compare("false", Object::Boolean(false));
+    }
+
+    #[test]
+    fn test_eval_prefix() {
+        eval_compare("!true", Object::Boolean(false));
+        eval_compare("!false", Object::Boolean(true));
+        eval_compare("!!true", Object::Boolean(true));
+        eval_compare("!!false", Object::Boolean(false));
+        eval_compare("!5", Object::Boolean(false));
+        eval_compare("!!5", Object::Boolean(true));
+
+        eval_compare("-32", Object::Integer(-32));
+        eval_compare("-1024", Object::Integer(-1024));
     }
 }
