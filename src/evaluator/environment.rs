af7757c826020eb76f5e39818f0b3b1efe99e380
CK <nankeen@users.noreply.github.com>
2020-04-30 17:24:51 +0100

    Implemented function calls
    fmt fix

--- src/evaluator/environment.rs
+++ src/evaluator/environment.rs
@@ -1,22 +1,35 @@
-#![allow(dead_code)]
 use super::Object;
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
+#[derive(PartialEq, Debug, Clone)]
 pub struct Environment {
     store: HashMap<String, Object>,
+    parent: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
     pub fn new() -> Environment {
         Environment {
             store: HashMap::new(),
+            parent: None,
         }
+    }
+
+    pub fn new_enclosed(outer: Rc<RefCell<Environment>>) -> Environment {
+        let mut env = Environment::new();
+        env.parent = Some(outer);
+        env
     }
 
     pub fn get(&self, name: &str) -> Option<Object> {
         match self.store.get(name) {
             Some(obj) => Some(obj.clone()),
-            None => None,
+            None => match self.parent {
+                Some(ref parent_env) => parent_env.borrow().get(name),
+                None => None,
+            },
         }
     }
 
